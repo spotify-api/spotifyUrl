2def36430b8a98cb2358bef1087f862d9936edb7
Sfue <50198413+abh80@users.noreply.github.com>
2020-12-11 17:35:54 +0530

    initial commit

---
+++ Structure.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpotifyUrl
+{
+    public class Structure
+    {
+        public AlbumStruct album { get; set; }
+        public ArtistStruct[] artists { get; set; }
+        public int disc_number { get; set; }
+        public int duration_ms { get; set; }
+        public external_urls external_urls { get; set; }
+        public string href { get; set; }
+        public string id { get; set; }
+        public bool is_local { get; set; }
+        public bool is_playable { get; set; }
+        public string name { get; set; }
+        public int popularity { get; set; }
+        public string preview_url { get; set; }
+        public int track_number { get; set; }
+        public string uri { get; set; }
+        public object dominantColor { get; set; }
+    }
+    public class AlbumStruct
+    {
+        public string album_type { get; set; }
+        public ArtistStruct[] artists { get; set; }
+        public external_urls external_urls { get; set; }
+        public string href { get; set; }
+        public string id { get; set; }
+        public Images[] images { get; set; }
+        public string name { get; set; }
+        public string release_date { get; set; }
+        public int total_tracks { get; set; }
+        public string type { get; set; }
+        public string uri { get; set; }
+
+    }
+    public class ArtistStruct
+    {
+        public external_urls external_urls { get; set; }
+        public string href { get; set; }
+        public string id { get; set; }
+        public string name { get; set; }
+        public string type { get; set; }
+        public string uri { get; set; }
+    }
+    public class external_urls
+    {
+        public string spotify { get; set; }
+    }
+    public class Images
+    {
+        public int height { get; set; }
+        public int width { get; set; }
+        public string url { get; set; }
+    }
+}
---
+++ UrlInfo.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+
+namespace SpotifyUrl
+{
+    public class UrlInfo
+
+    {
+        public async Task<Structure> GetData(string url)
+        {
+            var reg = new Regex(@"(http|https):\/\/open.spotify.com\/embed(\/track\/|\?uri=)(.*?)");
+            var rg = new Regex(@"(http|https):\/\/open.spotify.com\/track\/(.*?)");
+            if (reg.IsMatch(url))
+            {
+                var client = new HttpClient();
+                var res = await client.GetStringAsync(url);
+                var body = res.Split(new[] { $@"<script id=""resource"" type=""application/json"">" }, StringSplitOptions.None)[1].Split(new[] { "</script>" }, StringSplitOptions.None)[0];
+                var json = Uri.UnescapeDataString(body);
+                Structure d = JsonConvert.DeserializeObject<Structure>(json);
+                return d;
+            }
+            else if (rg.IsMatch(url))
+            {
+                var client = new HttpClient();
+                url = url.Split(new[] { "https://open.spotify.com/track/" }, StringSplitOptions.None)[1];
+
+                var res = await client.GetStringAsync("https://open.spotify.com/embed/track/" + url);
+                var body = res.Split(new[] { $@"<script id=""resource"" type=""application/json"">" }, StringSplitOptions.None)[1].Split(new[] { "</script>" }, StringSplitOptions.None)[0];
+                var json = Uri.UnescapeDataString(body);
+                Structure d = JsonConvert.DeserializeObject<Structure>(json);
+                return d;
+            }
+            else
+            {
+                throw new ArgumentException("The url is not a valid spotify track url");
+            }
+        }
+    }
+}
